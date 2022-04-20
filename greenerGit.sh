d31e6f83d6eba21c036b7481a7f0f54d96447f6c
PG-Momik <momik12339@gmail.com>
2022-04-20 23:55:12 +0545

    Comments added

--- greenerGit.sh
+++ greenerGit.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
+
+# this variable holds commit start date
 NEXT="2022-04-17"
+
+# set system date to variable date
 sudo date --set=$NEXT
+
+# navigate to directory with files
 cd directory
+
+# for loop to iterate over files
 for file in *.extention
 do 
+# $file variable has filename
+# add file 
  git add $file
+
+# commit file, commit message  e.g., "Finished abc.php"
  git commit -m "Finished ${file}"
+
+# increase $NEXT variable by 1 day
  NEXT=$(date +%Y-%m-%d -d "$NEXT + 1 day")
+
+# update system date
  sudo date --set="$NEXT"
+
+# might require sudo password, feeds password to promt
  echo password_literal | sudo -S command
 done
-
